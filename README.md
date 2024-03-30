e088bd6bc9ea9158c431d8279fe7b4f8e72d4f95
LaszloSimola <129189467+LaszloSimola@users.noreply.github.com>
2024-03-30 18:25:34 +0100

    The project is done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# MozaikJs
-Mozaik Js program
+# Mozaik Program
+Mozaik js program
+- A futtatás menete:
+  - Az oldalra érkezéskor az új gyűjtemény létrehozása gomb megnyomásakor megjelenik 3 mező, egy cím támakör és dátum.
+  - Ezután a Létrehozás gomb segítségével létrehozhatjuk a gyűjteményt ami meg is jelenik a hozzáadott gyűjtemények alatt
+  - ezután ezeket törölhetjük, átnevezhetjük és ezekhez új elemet adhatunk az erre alkalmas gombokkal
+  - az uj elem létrehozása gombbal egy benne lévő elemet adhatunk hozzá majd ezt az elemet áthelyezhetjük egy másik gyűjteménybe, átnevezhetjük és törölhetjük a legördülő lista és a megerősítés gomb segítségével
--- script.js
+++ script.js
@@ -1,114 +1,118 @@
-$(function() {
-    $("#dateInput").datepicker();
+    $(function() {
+        $("#dateInput").datepicker();
 
-    loadItems();
+        loadItems();
 
-    $("#createCollectionBtn").click(function() {
-        $("#collectionForm").toggle();
-    });
+        $("#createCollectionBtn").click(function() {
+            $("#collectionForm").toggle();
+        });
 
-    $("#createBtn").click(function() {
-        var name = $("#nameInput").val();
-        var description = $("#descriptionInput").val();
-        var date = $("#dateInput").val();
+        $("#createBtn").click(function() {
+            var name = $("#nameInput").val();
+            var description = $("#descriptionInput").val();
+            var date = $("#dateInput").val();
 
-        if (name === '' || description === '' || date === '') {
-            alert("Kérlek, töltsd ki az összes mezőt!");
-            return;
-        }
+            if (name === '' || description === '' || date === '') {
+                alert("Please fill out all fields!");
+                return;
+            }
 
-        var deleteButton = $("<button>").text("Törlés").addClass("btn btn-danger btn-sm ms-2");
-        var renameButton = $("<button>").text("Átnevezés").addClass("btn btn-primary btn-sm ms-2").attr("id", "renameButton");
-        var titleElement = $("<span>").addClass("title").text(name);
-        var addNewItemButton = $("<button>").text("Új elem hozzáadása").addClass("btn btn-success btn-sm ms-2").attr("id", "addNewItemButton");
-        var collectionItem = $("<li class=\"list-group-item\">").append("<strong>Cím:</strong> ", titleElement, ", <strong>Témakör:</strong> ", description, ", <strong>Dátum:</strong> ", date, deleteButton, renameButton, addNewItemButton);
-        $("#addedCollections").append(collectionItem);
-
-        deleteButton.css("margin-left", "10px");
-
-        saveItems();
-
-        deleteButton.click(function() {
-            collectionItem.remove();
+            var deleteButton = $("<button>").text("Törlés").addClass("btn btn-danger btn-sm ms-2 deleteButton");
+            var renameButton = $("<button>").text("Átnevezés").addClass("btn btn-primary btn-sm ms-2 renameButton");
+            var titleElement = $("<span>").addClass("title").text(name);
+            var addNewItemButton = $("<button>").text("Új elem hozzáadása").addClass("btn btn-success btn-sm ms-2 addNewItemButton");
+            var collectionItem = $("<li class=\"list-group-item\">").append("<strong>Title:</strong> ", titleElement, ", <strong>Description:</strong> ", description, ", <strong>Date:</strong> ", date, deleteButton, renameButton, addNewItemButton);
+            $("#addedCollections").append(collectionItem);
+            deleteButton.css("margin-left", "10px");
             saveItems();
         });
 
-        renameButton.click(function() {
-            var newTitle = prompt("Add meg az új címet:");
+        $(document).on("click", ".deleteButton", function() {
+            $(this).closest('li').remove();
+            saveItems();
+        });
+
+        $(document).on("click", ".renameButton", function() {
+            var titleElement = $(this).siblings('.title');
+            var newTitle = prompt("Adj meg egy új címet:");
             if (newTitle !== null && newTitle !== "") {
                 titleElement.text(newTitle); // Update the text of the title element
                 saveItems();
             }
         });
 
-        addNewItemButton.click(function() {
-            var newName = prompt("Add meg az új elem címét:");
+        $(document).on("click", ".addNewItemButton", function() {
+            var collectionItem = $(this).closest('li');
+            var newName = prompt("Új elem neve:");
             if (newName !== null && newName !== "") {
-                var newItemOptions = ['Áthelyezés', 'Átnevezés', 'Törlés']; // Example options for the dropdown
-                var select = $('<select>').addClass("");
+                var newItemOptions = ['Áthelyezés', 'Átnevezés', 'Törlés'];
+                var select = $('<select>').addClass('form-control');
                 for (var i = 0; i < newItemOptions.length; i++) {
                     $('<option>').val(newItemOptions[i]).text(newItemOptions[i]).appendTo(select);
                 }
                 var dropdownDiv = $("<div>").addClass("dropdown").append(select);
                 var newItem = $("<li>").addClass("list-group-item").text(newName);
-                var buttn = $('<button>').text("do it").addClass("btn btn-info");
+                var buttn = $('<button>').text("    Megerősítés").addClass("btn btn-info");
                 newItem.append(dropdownDiv);
                 collectionItem.append(newItem);
                 collectionItem.append(buttn);
                 saveItems();
 
+                // Attach event handler for the "do it" button inside the same scope
                 buttn.click(function() {
                     var selectedAction = select.val();
+                    var currentIndex = collectionItem.index();
                     if (selectedAction === "Áthelyezés") {
-                        // Add code to handle the "Áthelyezés" action here
+                        var newPosition = prompt("Enter the new position:");
+                        if (newPosition !== null && newPosition !== "") {
+                            var newIndex = parseInt(newPosition) - 1;
+                            var items = $("#addedCollections").children();
+                            if (newIndex >= 0 && newIndex < items.length) {
+                                var targetItem = items.eq(newIndex);
+                                if (!targetItem.is(collectionItem)) {
+                                    if (currentIndex < newIndex) {
+                                        collectionItem.insertAfter(targetItem);
+                                    } else {
+                                        collectionItem.insertBefore(targetItem);
+                                    }
+                                    saveItems();
+                                } else {
+                                    alert("The item is already in that position.");
+                                }
+                            } else {
+                                alert("Invalid position.");
+                            }
+                        }
                     } else if (selectedAction === "Átnevezés") {
-                        var newTitle = prompt("Add meg az új címet:");
+                        var newTitle = prompt("Adj meg egy új címet:");
                         if (newTitle !== null && newTitle !== "") {
                             newItem.text(newTitle); // Update the text of the list item
                             newItem.append(dropdownDiv);
                             collectionItem.append(newItem);
                             collectionItem.append(buttn);
+                            saveItems();
                         }
-                    }else if(selectedAction === "Törlés"){
+                    } else if (selectedAction === "Törlés") {
                         newItem.remove();
                         buttn.remove();
+                        saveItems();
                     }
                     return false;
                 });
             }
         });
-    });
 
-
-    function loadItems() {
-        var savedItems = localStorage.getItem("savedItems");
-        if (savedItems) {
-            $("#addedCollections").html(savedItems);
-            // Reattach event handlers for dynamically created elements
-            attachEventHandlers();
+        function loadItems() {
+            var savedItems = localStorage.getItem("savedItems");
+            if (savedItems) {
+                $("#addedCollections").html(savedItems);
+            }
         }
-    }
 
-    function saveItems() {
-        var itemsHtml = $("#addedCollections").html();
-        localStorage.setItem("savedItems", itemsHtml);
-    }
+        function saveItems() {
+            var itemsHtml = $("#addedCollections").html();
+            localStorage.setItem("savedItems", itemsHtml);
+        }
 
-    // Function to attach event handlers for dynamically created elements
-    function attachEventHandlers() {
-        // Event handler for delete buttons
-        $(".btn-danger").click(function() {
-            $(this).parent().remove();
-            saveItems();
-        });
 
-    }
-
-    // Create the button and append it to the DOM
-    var addButton = $('<button>').text('Add Item').addClass('btn btn-primary').attr('id', 'addButton');
-    $('#listContainer').append(addButton);
-    addButton.click(function() {
-        // Trigger the click event of the addNewItemButton when this button is clicked
-        $('#addNewItemButton').trigger('click');
     });
-});
